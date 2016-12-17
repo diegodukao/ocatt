4783d74687d083efcfcc9ac71c4c8031fd65c456
Diego Dukão <diegodukao@gmail.com>
2016-12-17 15:43:36 -0200

    creating gameworld

--- main.py
+++ main.py
@@ -7,7 +7,25 @@ kivy.require('1.9.1')
 
 
 class TestGame(Widget):
-    pass
+    def __init__(self, **kwargs):
+        super(TestGame, self).__init__(**kwargs)
+        self.gameworld.init_gameworld([], callback=self.init_game)
+
+    def init_game(self):
+        self.setup_states()
+        self.set_state()
+
+    def setup_states(self):
+        self.gameworld.add_state(
+            state_name='main',
+            systems_added=[],
+            systems_removed=[],
+            systems_paused=[],
+            systems_unpaused=[],
+            screenmanager_screen='main')
+
+    def set_state(self):
+        self.gameworld.state = 'main'
 
 
 class OcattApp(App):
